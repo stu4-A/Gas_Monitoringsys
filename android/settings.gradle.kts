a75557acf521a30035480cd100baa531a9706710
stu4-A <stuartowani29@gmail.com>
2025-11-01 17:04:33 +0300

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -4,11 +4,6 @@ pluginManagement {
         google()
         mavenCentral()
     }
-
-    plugins {
-        id("com.android.application") version "8.2.0"
-        id("org.jetbrains.kotlin.android") version "2.0.21"
-    }
 }
 
 rootProject.name = "gasmonitoringsys"
