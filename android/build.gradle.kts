bfb5eabff04de98b86153d04520d1e79951bbb7f
stu4-A <stuartowani29@gmail.com>
2025-11-01 17:16:05 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,11 +1,14 @@
-// Top-level project build.gradle.kts
-plugins {
-    // Kotlin Gradle plugin
-    kotlin("android") version "2.0.21" apply false
-    // Android Gradle plugin
-    id("com.android.application") version "8.2.0" apply false
-    // Flutter Gradle plugin
-    id("dev.flutter.flutter-gradle-plugin") version "0.1.15" apply false
+// android/build.gradle.kts (Project-level)
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
+    }
 }
 
 allprojects {
