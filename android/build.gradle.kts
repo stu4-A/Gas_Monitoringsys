e33d45c6c44039cbc0b6a2f6d7e2203d593507d1
stu4-A <stuartowani29@gmail.com>
2025-11-01 16:56:28 +0300

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,45 +1,19 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
-android {
-    namespace = "com.example.gasmonitoringsys"
-    compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
-
-    defaultConfig {
-        applicationId = "com.example.gasmonitoringsys"
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+// Top-level build.gradle.kts
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
     }
 
-    buildTypes {
-        getByName("release") {
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-
-    // ✅ Compile options for Java 11 + core library desugaring
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-        isCoreLibraryDesugaringEnabled = true // ✅ Kotlin DSL syntax
-    }
-
-    kotlinOptions {
-        jvmTarget = "11" // ✅ Kotlin DSL requires string
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
     }
 }
 
-flutter {
-    source = "../.."
-}
-
-dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
